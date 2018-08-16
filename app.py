b641b9f1f9c000affc460587f76b0342ab01e421
JiaheLiang <41764851+JiaheLiang@users.noreply.github.com>
2018-08-16 16:43:23 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -14,6 +14,32 @@ mysql.init_app(app)
 @app.route("/")
 def main():
     return render_template('login.html')
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputName']
+        _password = request.form['inputPassword']
+               
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/user')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')          
+
+    except Exception as e:
+        return render_template('sign.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+	
 @app.route('/showSignup')
 def showSignup():
     return render_template('sign.html')
@@ -23,12 +49,14 @@ def signup():
 		_name = request.form['inputName']
 		_email = request.form['inputEmail']
 		_password = request.form['inputPassword']
- 
-		if _name and _email and _password:
+		_age = request.form['inputAge']
+		if _age < 18:
+			return rendere_template['errorage.html']
+		elif _name and _email and _password and _age:
 			conn = mysql.connect()
 			cursor = conn.cursor()
-			_hashed_password = generate_password_hash(_password)
-			cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+			hashed_password = generate_password_hash(_password)
+			cursor.callproc('sp_createUser',(_name,_email,_hashed_password,_age))
 			data = cursor.fetchall()
 			if len(data) is 0:
 				conn.commit()
@@ -43,7 +71,15 @@ def signup():
 	finally:
 		cursor.close() 
 		conn.close()
-	
+
+@app.route('/user')
+def user():
+    return render_template('user.html')		
+
+@app.route('/logout')	
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 	
 if __name__ == "__main__":
     app.run()
